8f362a2eaa616a705aea9dcb14babb9c3dcda2c2
Dobrobatko Dmytro <dobrobatka@gmail.com>
2024-10-06 14:40:11 +0300

    fix promises

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -9,20 +9,28 @@ function handleSubmit(e) {
   e.preventDefault();
   const delay = e.target.delay.value;
   const state = e.target.state.value;
-  setTimeout(() => {
-    if (state === 'fulfilled') {
-      Promise.resolve(
-        iziToast.success({
-          message: `Fulfilled promise in ${delay}ms`,
-          position: 'topRight',
-        })
-      );
-    } else
-      Promise.reject(
-        iziToast.error({
-          message: `Rejected promise in ${delay}ms`,
-          position: 'topRight',
-        })
-      );
-  }, delay);
+
+  const promise = new Promise((res, rej) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        res(delay);
+      } else {
+        rej(delay);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(delay => {
+      iziToast.success({
+        message: `Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        message: `Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    });
 }
